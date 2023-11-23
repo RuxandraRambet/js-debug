f8a0d1bd9ad24cdaeec2591c233e80c73051c386
RuxandraRambet <ruxandraramb@gmail.com>
2023-11-23 22:30:58 +0100

    feat: bundle02

--- bundle-02/main.js
+++ bundle-02/main.js
@@ -23,6 +23,27 @@ function checkAge() {
 }
 checkAge();
 
+/*
+Lo scopo di questa funzione dovrebbe essere quello di stabilire se l'utenete 
+è magiorenne o no. In tal senso invece che dichiarare una variabile myAge all'interno
+della funzione, potremmo inserire myAge come argomento della funzione.
+Un'errore è quello di utilizzare const invece di let per dichiarare la variabile message in quanto
+in seguito gli verrà assegnato un nuovo valore. 
+Inoltre, nella funzione manca il return di message.
+*/
+// SOLUZIONE CORRETTA
+// function checkAge(myAge) {
+//     let message = '';
+
+//     if (myAge < 18) {
+//         message = `Sei troppo giovane! Hai ${myAge} anni!`;
+//     } else {
+//         message = 'Hai più di 18 anni!';
+//     }
+//     return message;
+// }
+// checkAge();
+
 // ESERCIZIO 2
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
@@ -30,6 +51,16 @@ function printColorsNumber() {
 }
 printColorsNumber();
 
+/*
+In questo caso mi sembra ci sia un'errore di scrittura : LENGTH e non LENGHT
+*/
+
+// SOLUZIONE CORRETTA
+// function printColorsNumber() {
+//     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+//     console.log(`Nella mia palette ci sono ${colors.length} colori!`);
+// }
+// printColorsNumber();
 
 // ESERCIZIO 3
 function addNumbers() {
@@ -40,6 +71,20 @@ function addNumbers() {
 }
 addNumbers();
 
+/*
+In questo caso, considerando che all'utente viene chiesto di inserire un numero tramite prompt,
+dovremmo assicurarci che il valore passato sia un numero. Per fare questo possiamo utilizzare
+Number(), parseInt(), parseFloat().
+*/
+
+//SOLUZIONE CORRETTA
+// function addNumbers() {
+//     const userNumber = Number(prompt('Inserisci un numero'));
+//     const total = userNumber + 12;
+
+//     console.log(`Il risultato finale è ${total}`);
+// }
+// addNumbers();
 
 // ESERCIZIO 4
 function checkAccess() {
@@ -59,6 +104,29 @@ function checkAccess() {
     }
 }
 checkAccess();
+
+/*
+In questo caso l'errore è l'utilizzo di '' per scrivere gli operatori booleani.
+Scritti in questo modo diventano delle stringhe.
+*/
+// SOLUZIONE CORRETTA
+// function checkAccess() {
+//     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+//     let grantAccess = false;
+
+//     if (addresses.includes(userEmail)) {
+//         grantAccess = true;
+//     }
+
+//     if (grantAccess === true) {
+//         console.log('Accesso consentito!');
+//     } else {
+//         console.log('Accesso negato!');
+//     }
+// }
+// checkAccess();
 
 
 // ESERCIZIO 5 (suggerimento: c'è un solo errore)
@@ -89,7 +157,40 @@ function checkAccessImproved() {
     }
     checkAccessImproved();
 
+/*
+In questo caso il primo errore che vedo è che manca la } per chiudere la funzione.
+Inoltre, anche in questo caso i valori true e false sono scritti con le '', ed essendo
+dei valori booleani gli apici non servono.
+Infine la condizione per la stampa del messaggio in console andrebbe scritta fuori dal ciclo for
+così che il messaggio venga stampato una volta sola.
+*/
 
+// SOLUZIONE CORRETTA
+// function checkAccessImproved() {
+//     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+//     let grantAccess = false;
+
+//     for (let i = 0; i < addresses.length; i++) {
+//         const email = addresses[i];
+
+//         if (userEmail.length > 5) {
+
+//             if (email === userEmail) {
+//                 grantAccess = true;
+//             }
+//         }
+//     }
+    
+//     if (grantAccess) {
+//         console.log('Accesso consentito!');
+//     } else {
+//         console.log('Accesso negato!');
+//     }
+// }    
+// checkAccessImproved();
 
 
 
