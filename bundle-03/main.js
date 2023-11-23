0b41636f3e3d8549782305f21b612f14e484f302
RuxandraRambet <ruxandraramb@gmail.com>
2023-11-23 23:05:39 +0100

    feat: bundle03

--- bundle-03/main.js
+++ bundle-03/main.js
@@ -84,3 +84,93 @@ console.log(dieselCars);
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+
+/*
+Lo scopo di questo codice è di dividere un'array di oggetti, in questo caso macchine, per tipo di carburante
+e in seguito stampare il risultato in console.
+
+Errore 1: all'interno dell'array di oggetti, a riga 60, manca la "," dopo }
+Errore 2: all'interno della const gasolineCars c'è un errore di scrittura dellal arrow function 
+dovrebbe essere => non >=.
+Errore 3: a riga 69, c'è un errore nella struttura della arrow function che dovrebbe avere un return
+prima di auto.type oppure dovrebbe essere scritta su una riga sola come nel caso di gasolineCars dove 
+il return è implicito.
+Errore 4: per poter escludere sia le auto a benzina che quelle a diesel all'interno dell'ultima arrow function
+dovremmo utilizzare l'operatore binario &&(richiede che entrambi i valori siano veri) invece di ||.
+Errore 5: considerando che javascript è case sensitive dovremmo utilizzare il metodo delle stringhe
+toLowerCase() per sistemare il fatto che all'interno dell'array il tipo di carburante a volte viene scritto 
+con lettera maiuscola altre volte tutto minuscolo.
+*/ 
+
+//SOLUZIONE CORRETTA
+// const cars = [
+//     {
+//         manufacturer: 'Ford',
+//         model: 'Fiesta',
+//         type: 'diesel'
+//     },
+//     {
+//         manufacturer: 'Audi',
+//         model: 'A1',
+//         type: 'benzina'
+//     },
+//     {
+//         manufacturer: 'Volkswagen',
+//         model: 'Golf',
+//         type: 'Benzina'
+//     },
+//     {
+//         manufacturer: 'Fiat',
+//         model: 'Panda',
+//         type: 'metano'
+//     },
+//     {
+//         manufacturer: 'Fiat',
+//         model: 'Multipla',
+//         type: 'GPL'
+//     },
+//     {
+//         manufacturer: 'Tesla',
+//         model: 'Model 3',
+//         type: 'elettrico'
+//     },
+//     {
+//         manufacturer: 'Volkswagen',
+//         model: 'Polo',
+//         type: 'benzina'
+//     },
+//     {
+//         manufacturer: 'Ford',
+//         model: 'Kuga',
+//         type: 'Diesel'
+//     },
+//     {
+//         manufacturer: 'Seat',
+//         model: 'Ibiza',
+//         type: 'metano'
+//     },
+//     {
+//         manufacturer: 'Audi',
+//         model: 'R8',
+//         type: 'Benzina'
+//     },
+// ];
+
+// const gasolineCars = cars.filter( (auto) => auto.type.toLowerCase() === 'benzina');
+
+// const dieselCars = cars.filter( (auto) => auto.type.toLowerCase() === 'diesel');
+
+// const otherCars = cars.filter( (auto) => {
+//     return auto.type.toLowerCase() !== 'benzina' && auto.type.toLowerCase() !== 'diesel';
+// });
+
+// console.log('Auto a benzina');
+// console.log('*******************************');
+// console.log(gasolineCars);
+
+// console.log('Auto a diesel');
+// console.log('*******************************');
+// console.log(dieselCars);
+
+// console.log('Tutte le altre auto');
+// console.log(otherCars);
